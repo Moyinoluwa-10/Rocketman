06fac178074ca963d3526339821cc2a67025ca3d
Moyinoluwa-10 <104604245+Moyinoluwa-10@users.noreply.github.com>
2022-06-03 04:28:27 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// For Nav Section
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+}
